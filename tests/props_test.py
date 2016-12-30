859a9aa684b793a31dbf0b1f8e559d5cd40a152e
Stephen Cagle <stephen@read-line.com>
2016-12-30 09:33:42 -0800

    Enumerate through choices of property values

--- tests/props_test.py
+++ tests/props_test.py
@@ -1,10 +1,23 @@
 from fixture import GeneratorTest
 from google.appengine.ext import testbed, ndb
+import gaend.generator as generator
+import re
 
 
 class PropsTest(GeneratorTest):
 
-    def testLotsaModelsGenerated(self):
+    def testEntityToPropsAndBack(self):
         for klass in self.klasses:
-            k = klass._get_kind()
-            assert ndb.Model._lookup_model(k) == klass, klass
+            # Create entity1 of this klass
+            kind = klass._get_kind()
+            # 'BlobPropertyRepeatedModel' -> 'Blob'
+            prop_name = re.match(r'(.+)Property', kind).group(1)
+            prop_name = prop_name[0].lower() + prop_name[1:]
+            prop_klass = klass._properties[prop_name + 'Property'].__class__
+            prop_type = generator.PROPERTIES[prop_klass]
+            for v in generator.VALUES[prop_type]:
+                print v
+
+            # Convert entity1 to props form
+            # Create entity2 derived from props
+            # Assure that first entity1 is equal to entity2
