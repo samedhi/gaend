93014fab3f00a4a7cfd46766752b794766a47d69
Stephen Cagle <stephen@read-line.com>
2016-12-31 09:54:38 -0800

    Build a Entity for every permutation

--- tests/props_test.py
+++ tests/props_test.py
@@ -12,11 +12,20 @@ class PropsTest(GeneratorTest):
             kind = klass._get_kind()
             # 'BlobPropertyRepeatedModel' -> 'Blob'
             prop_name = re.match(r'(.+)Property', kind).group(1)
-            prop_name = prop_name[0].lower() + prop_name[1:]
-            prop_klass = klass._properties[prop_name + 'Property'].__class__
+            prop_name = prop_name[0].lower() + prop_name[1:] + 'Property'
+            prop_klass = klass._properties[prop_name].__class__
             prop_type = generator.PROPERTIES[prop_klass]
             for v in generator.VALUES[prop_type]:
-                print v
+                if re.search('Repeated', klass._get_kind()):
+                    v = [v]
+                else:
+                    v = generator.VALUES[prop_type]
+                for i, fx in enumerate(v):
+                    if callable(fx):
+                        v[i] = fx()
+                if not re.search('Repeated', klass._get_kind()):
+                    v = v[0]
+                klass(**{prop_name:v})
 
             # Convert entity1 to props form
             # Create entity2 derived from props
