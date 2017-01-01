7b0b339b835be1ddbfd64a589fbf5d1e6c49d5b3
Stephen Cagle <stephen@read-line.com>
2017-01-01 08:16:30 -0800

    Create the endpoints

--- gaend/endpoint.py
+++ gaend/endpoint.py
@@ -1,6 +1,28 @@
-from flask import Flask, jsonify, request
 from google.appengine.ext import testbed, ndb
-import props
+import props as gprops
+
+
+def get(urlsafekey_or_entity):
+    """Read a existent entity
+
+    {'kind': <ClassNameOfEntity>,
+     'key': <urlsafekey>}
+
+    Returns the above dictionary as a json object where:
+    1) All `ndb.Key` entity properties are converted to their urlsafe keys.
+    2) All `datetime`, `date`, and `time` objects are converted to iso8601.
+    3) Json and Pickled are converted to sub objects or list.
+    4) ndb.GeoPt(X,Y) becomes `{'lat': <latitude>, 'lon': <longitude>}`
+    5) Every other property is directly copied.
+    """
+    if isinstance(urlsafekey_or_entity, ndb.Model):
+        entity = urlsafekey_or_entity
+        assert entity.key, entity
+    else:
+        key = ndb.Key(urlsafe=urlsafekey)
+        entity = key.get()
+    return gprops.entity_to_props(entity)
+
 
 def post(props):
     """Create a new entity
@@ -16,24 +38,10 @@ def post(props):
 
 
     """
-    pass
-
-
-def get(urlsafekey):
-    """Read a existent entity
-
-    {'kind': <ClassNameOfEntity>,
-     'key': <urlsafekey>}
-
-    Returns the above dictionary as a json object where:
-    1) All `ndb.Key` entity properties are converted to their urlsafe keys.
-    2) All `datetime`, `date`, and `time` objects are converted to iso8601.
-    3) Json, Pickled, and *Structured are converted to sub-dictionaries.
-    4) ndb.GeoPt(X,Y) are converted to `{'x': X, 'y': Y}`
-    5) Every other property is directly copied.
-
-    """
-    pass
+    klass = ndb.Model._lookup_model(props['kind'])
+    entity = klass(**props)
+    entity.put()
+    return get(entity)
 
 
 def put(urlsafekey, props):
@@ -44,9 +52,13 @@ def put(urlsafekey, props):
     Datastore using the above post() method. In this way all Datastore
     writes always go through the post() method.
     """
-    pass
+    entity = ndb.Key(urlsafe=urlsafekey).get()
+    updated_props = gprops.entity_to_props(entity)
+    for k, v in props.items():
+        updated_props[k] = v
+    return post(updated_props)
 
 
 def delete(urlsafekey):
     """Destroy a entity"""
-    pass
+    ndb.Key(urlsafekey=urlsafekey).delete()
