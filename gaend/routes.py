aacec2c457c69e5a072b5be00e231b40f35a6d74
Stephen Cagle <stephen@read-line.com>
2017-01-01 08:45:24 -0800

    Support both new and old 'REST'

--- gaend/routes.py
+++ gaend/routes.py
@@ -3,6 +3,8 @@ import endpoint
 import js
 
 
+@APP.route('/<model>/<urlsafekey>', methods=['GET'])
+@APP.route('/<urlsafekey>', methods=['GET'])
 def get(urlsafekey):
     props = endpoint.get(urlsafekey)
     json = js.props_to_js(props)
@@ -11,18 +13,24 @@ def get(urlsafekey):
     return resp
 
 
+@APP.route('/<model>/<urlsafekey>', methods=['PUT'])
+@APP.route('/<urlsafekey>', methods=['PUT'])
 @ndb.transactional(xg=True)
 def put(json):
     props = js.js_to_props(json)
     return endpoint.put(props)
 
 
+@APP.route('/<model>', methods=['POST'])
+@APP.route('/', methods=['POST'])
 @ndb.transactional(xg=True)
 def post(json):
     props = js.js_to_props(json)
     return endpoint.post(props)
 
 
-def delete(urlsafekey):
+@APP.route('/<model>/<urlsafekey>', methods=['DELETE'])
+@APP.route('/<urlsafekey>', methods=['DELETE'])
+def delete(model, urlsafekey):
     endpoint.delete(urlsafekey)
     return "SUCCESS"
