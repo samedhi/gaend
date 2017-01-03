d6f0d9ddf18c58244c8b2b2488fc49a1e8d56211
Stephen Cagle <stephen@read-line.com>
2017-01-03 15:12:31 -0800

    Includes  (Python) examples of prop values docs

--- gaend/docs.py
+++ gaend/docs.py
@@ -1,10 +1,12 @@
 from google.appengine.ext import ndb
 from flask import request, Response
+from gaend import generator
 from jinja2 import Environment, FileSystemLoader
 from state import APP
+import logging
 
 class User(ndb.Model):
-    name = ndb.StringProperty()
+    name = ndb.DateTimeProperty()
     email = ndb.StringProperty()
 
 env = Environment(loader=FileSystemLoader('templates'))
@@ -13,6 +15,15 @@ env = Environment(loader=FileSystemLoader('templates'))
 def docs():
     o = {}
     for klass in ndb.Model._kind_map.values():
-        o[klass.__name__] = klass._properties
+        pd = {}
+        for k, p in klass._properties.iteritems():
+            klass = p.__class__
+            kind = generator.PROPERTIES[klass]
+            values = generator.VALUES[kind]
+            logging.error(values)
+            pd[k] = {'propClass': p,
+                     'vals': values}
+        o[klass.__name__] = {'modelClass': klass,
+                             'properties': pd}
 
     return env.get_template('documentation.html').render(klasses=o)
