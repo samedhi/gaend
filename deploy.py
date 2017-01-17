c7b21e09f07a125c2be1affc477578692a3c936b
Stephen Cagle <stephen@read-line.com>
2017-01-17 14:42:16 -0800

    Commit change as well

--- deploy.py
+++ deploy.py
@@ -2,10 +2,15 @@
 import re
 import subprocess
 
+PORCELAIN = 'git status --porcelain'
+clean = subprocess.check_output(PORCELAIN.split(' '))
+assert clean == "", clean
+
 with open('setup.py', 'r') as setup:
     c = setup.read()
-    r = re.search("version='\d+\.\d+\.\d+.dev(\d+)", c)
-    i = int(r.group(1))
+    r = re.search("version='(\d+\.\d+\.\d+.dev)(\d+)", c)
+    s = r.group(1)
+    i = int(r.group(2))
     i_inc = i + 1
     setup_inc = re.sub("dev%s" % i, "dev%s" % i_inc, c)
 
@@ -15,5 +20,8 @@ with open('setup.py', 'w+') as setup:
 BUILD_WHEEL = 'python setup.py sdist bdist_wheel'
 subprocess.check_output(BUILD_WHEEL.split(' '))
 
-PIP_PUSH = 'twine upload dist/gaend-*'
-subprocess.check_output(PIP_PUSH.split(' '))
+# PIP_PUSH = 'twine upload dist/gaend-*'
+# subprocess.check_output(PIP_PUSH.split(' '))
+
+# COMMIT = 'git add setup.py; git commit -m "pip deploy version %s"' % (s + i_inc)
+# subprocess.check_output(COMMIT.split(' '))
