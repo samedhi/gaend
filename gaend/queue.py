1e2bbf56e55caa3ca8bf04814ef5e387189dd06c
Stephen Cagle <stephen@read-line.com>
2017-01-18 17:41:10 -0800

    Gaend defaults to writting changes to task queue

--- gaend/queue.py
+++ gaend/queue.py
@@ -1,3 +1,32 @@
+from google.appengine.ext import ndb
+from state import APP
+
+
 # If you don't want gaend to use the default queue, override the
 # GAEND_QUEUE variable.
 GAEND_QUEUE = 'default'
+
+# This is a list of functions that will be called in the task queue
+# when a datastore record is added/updated.
+ON_PUT = []
+
+# This is a list of functions that will be called in the task queue
+# when a datastore record is deleted.
+ON_DELETE = []
+
+
+def call_every_handler(handlers, request):
+    for fx in handlers:
+        fx(request)
+
+
+@APP.route('/gaend/put/<urlkey>', methods=['POST'])
+def puter():
+    ndb.transaction(lambda: call_every_handler(ON_MODIFY, request), xg=True)
+    return 200
+
+
+@APP.route('/gaend/delete/<urlkey>', methods=['POST'])
+def deleter():
+    ndb.transaction(lambda: call_every_handler(ON_DELETE, request), xg=True)
+    return 200
